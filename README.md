d2e1dc567176604ec48a240ff89f708ed94abb63
Wu Kai <koprvhdix@gmail.com>
2024-03-06 09:49:57 +0800

    fix

---
+++ README.md
@@ -0,0 +1,6 @@
+# Detecting entanglement structure by machine learning
+
+## 任务列表
+- [x] 选取 5 个矩阵最大元素。
+- [ ] 进行求解系数。
+- [ ] 完成数据的整理
--- bin/create_dataset.py
+++ bin/create_dataset.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-from partition_tools import generate_k_producible_partitions
 
-
-def random_state(n):
+def random_state(n_qubit):
+    n = 2**n_qubit
     real_current_matrix = np.random.rand(n, n)
     real_current_matrix = np.matrix(real_current_matrix)
 
@@ -16,11 +15,31 @@ def random_state(n):
     return np.array(state / state.trace())
 
 
-def create_dataset(n_qubit, num_of_quantum_state, partition_list):
+def create_dataset(n_qubit, num_of_quantum_state):
     matrix_size = 2 ** n_qubit
     matrix_list = list()
     for i in range(num_of_quantum_state):
         current_matrix = random_state(matrix_size)
+        matrix_list.append(current_matrix)
+
+
+def select_elements(matrix, num_element):
+    matrix_copy = np.copy(matrix)
+
+    # Calculate the magnitude of each element
+    magnitude = np.abs(matrix_copy)
+
+    # Set the lower triangular elements to 0
+    magnitude[np.tril_indices(magnitude.shape[0])] = 0
+
+    # Get the indices of the n elements with the largest magnitude
+    indices = np.dstack(np.unravel_index(np.argsort(magnitude.ravel()), magnitude.shape))[0]
+
+    # Filter out the indices of the diagonal elements
+    indices = [index for index in indices if index[0] != index[1]]
+
+    # Return the indices of the n non-diagonal elements with the largest magnitude
+    return indices[-num_element:]
 
 
 if __name__ == "__main__":
@@ -29,4 +48,9 @@ if __name__ == "__main__":
     n_qubit = 4
     num_of_quantum_state = 5000
 
-    partition_list = generate_k_producible_partitions(n_qubit, k)
+    # partition_list = generate_k_producible_partitions(n_qubit, k)
+
+    current_matrix = random_state(n_qubit)
+    print(current_matrix)
+    elements = select_elements(current_matrix, 5)
+    print(elements)
