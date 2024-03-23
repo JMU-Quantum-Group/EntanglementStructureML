274a1b08727a4b9c560d79e417ca941964b48d87
Wu Kai <koprvhdix@gmail.com>
2024-03-23 21:33:14 +0800

    add prod 2

--- bin/tpot_train.py
+++ bin/tpot_train.py
@@ -7,23 +7,27 @@ full_sep_labels = np.load('full_sep_labels.npy', allow_pickle=True)
 part_3_data = np.load('part_3_pure_states.npy', allow_pickle=True)
 part_3_labels = np.load('part_3_pure_labels.npy', allow_pickle=True)
 
-matrices = np.concatenate((full_sep_data, part_3_data))
-labels = np.concatenate((full_sep_labels, part_3_labels))
+prod_2_data = np.load('prod_2_states.npy', allow_pickle=True)
+prod_2_labels = np.load('prod_2_labels.npy', allow_pickle=True)
+
+matrices = np.concatenate((full_sep_data, part_3_data, prod_2_data))
+labels = np.concatenate((full_sep_labels, part_3_labels, prod_2_labels))
 
 train_indices = np.arange(matrices.shape[0])
 np.random.shuffle(train_indices)
-
 matrices = matrices[train_indices]
 labels = labels[train_indices]
 
 # start test data
 full_sep_test_data = np.load('full_sep_states_test.npy', allow_pickle=True)
 full_sep_test_labels = np.load('full_sep_labels_test.npy', allow_pickle=True)
-part_3_test_data = np.load('part_3_pure_states_test.npy', allow_pickle=True)
-part_3_test_labels = np.load('part_3_pure_labels_test.npy', allow_pickle=True)
+part_3_test_data = np.load('part_3_states_test.npy', allow_pickle=True)
+part_3_test_labels = np.load('part_3_labels_test.npy', allow_pickle=True)
+prod_2_test_data = np.load('prod_2_states_test.npy', allow_pickle=True)
+prod_2_test_labels = np.load('prod_2_labels_test.npy', allow_pickle=True)
 
-test_matrices = np.concatenate((full_sep_test_data, part_3_test_data))
-test_labels = np.concatenate((full_sep_test_labels, part_3_test_labels))
+test_matrices = np.concatenate((full_sep_test_data, part_3_test_data, prod_2_test_data))
+test_labels = np.concatenate((full_sep_test_labels, part_3_test_labels, prod_2_test_labels))
 
 indices = np.arange(test_matrices.shape[0])
 np.random.shuffle(indices)
@@ -36,4 +40,4 @@ test_matrices = test_matrices.reshape(test_matrices.shape[0], test_matrices.shap
 tpot = TPOTClassifier(generations=5, population_size=20, verbosity=2, random_state=42)
 tpot.fit(matrices, labels)
 print(tpot.score(test_matrices, test_labels))
-tpot.export('tpot_digits_pipeline.py')
+tpot.export('tpot_data_pipeline.py')
